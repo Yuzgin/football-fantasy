b04e7c1fd380c88dbcc758a23a0010e4b55281cc
Yuzgin <andrewy133@hotmail.co.uk>
2025-05-15 18:15:05 +0100

    WD

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -55,7 +55,7 @@ const Home = () => {
         <div className="table-wrapper">
           <StatsTableGoals
             players={playersGoals}
-            stat="Goals"
+            goal="Goals"
             title="Top Scorers"
           />
         </div>
