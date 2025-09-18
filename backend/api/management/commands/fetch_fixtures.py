f783dc1f7a8420ae39755eb35ebfa8b62e7036fe
Andrew Yuzgin <andrewy133@hotmail.co.uk>
2025-09-18 14:05:20 +0100

    Fixtures

--- backend/api/management/commands/fetch_fixtures.py
+++ backend/api/management/commands/fetch_fixtures.py
@@ -10,12 +10,12 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # List of URLs to fetch fixtures from
         urls = [
-            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cm4gzn84r00km4psx58wzhakc/fixtures',
-            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cm4gzmuvt00kj4psx49yaps79/fixtures',
-            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cm4gzln0k00k74psxubhpg9o1/fixtures',
-            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cm4gzlwup00ka4psxulj0k7il/fixtures',
-            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cm4gzm5kh00kd4psxm61q3xko/fixtures',
-            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cm4gzmemq00kg4psxv8u2vwdk/fixtures'
+            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cmf0vzg3w00jyo301fztuxggp/fixtures',
+            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cmf155vyo00leo301skln6l5t/fixtures',
+            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cmf1574ml00lyo301xxb0wycg/fixtures',
+            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cmf15929d00mmo3017r1xlhva/fixtures',
+            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cmf15aqxa00n6o301og4g9k63/fixtures',
+            'https://sports-admin.yorksu.org/api/clst1o9lv0001q5teb61pqfyy/leagues/cmf15c4dc00nqo301av3tyodt/fixtures',
         ]
 
         # Get today's date
